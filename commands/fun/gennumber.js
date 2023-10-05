56f338100713a015df2e0a53354e8b2beb62eb62
Jaison-Rico <jaih90@aol.com>
2023-10-04 22:23:37 -0500

    Generar un numero random del 1 al 1000

---
+++ commands/fun/gennumber.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('numero')
+		.setDescription('Genera un numero random del 1 al 1000!'),
+	async execute(interaction) {
+		const numeroAleatorio = Math.floor(Math.random() * 1000) + 1;
+    
+    await interaction.reply(`El número aleatorio es: ${numeroAleatorio}`);
+	},
+};
