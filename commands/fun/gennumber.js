2b2bc803e37e055b2c12d43067db3443118eb13c
Jaison-Rico <jaih90@aol.com>
2023-10-04 22:51:15 -0500

    se le agrego categoria a gennumber

--- commands/fun/gennumber.js
+++ commands/fun/gennumber.js
@@ -4,6 +4,7 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('numero')
 		.setDescription('Genera un numero random del 1 al 1000!'),
+        category: 'fun',
 	async execute(interaction) {
 		const numeroAleatorio = Math.floor(Math.random() * 1000) + 1;
     
