42fb86cf29cd3166aff8cc39c68d4fdbc9f30a78
Jaison-Rico <jaih90@aol.com>
2023-10-04 22:30:37 -0500

    Traduccion del reload

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -3,17 +3,17 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('reload')
-		.setDescription('Reloads a command.')
+		.setDescription('Reinicia un comando.')
 		.addStringOption(option =>
 			option.setName('command')
-				.setDescription('The command to reload.')
+				.setDescription('El comando a reiniciar.')
 				.setRequired(true)),
 	async execute(interaction) {
 		const commandName = interaction.options.getString('command', true).toLowerCase();
 		const command = interaction.client.commands.get(commandName);
 
 		if (!command) {
-			return interaction.reply(`There is no command with name \`${commandName}\`!`);
+			return interaction.reply(`No hay ningun comando con ese nombre \`${commandName}\`!`);
 		}
 
 		delete require.cache[require.resolve(`../${command.category}/${command.data.name}.js`)];
@@ -25,7 +25,7 @@ module.exports = {
 	        await interaction.reply(`El comando \`${newCommand.data.name}\` fue reiniciado!`);
 		} catch (error) {
 	        console.error(error);
-	        await interaction.reply(`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``);
+	        await interaction.reply(`Hubo un error mientras se reiniciaba el comando \`${command.data.name}\`:\n\`${error.message}\``);
 		}
 	},
 };
